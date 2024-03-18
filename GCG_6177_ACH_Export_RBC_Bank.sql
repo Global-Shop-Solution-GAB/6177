87465e6d962a103625f958e05880e37b43aef626
Service Web <sw@gssmail.com>
2024-03-18 11:13:56 -0500

    Commit changes in C:\Websites\Git\emarlow\6177\GCG_6177_ACH_Export_RBC_Bank.sql

---
+++ GCG_6177_ACH_Export_RBC_Bank.sql
@@ -0,0 +1,11 @@
+-- Database: GLOBALTST Metadata version: 2 Exported: Nov 30, 2020
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6177_ACH_RBC_EXP" USING 'GCG_6177_ACH_RBC_EXP.mkd' PAGESIZE=4096 (
+ "VENDOR" CHAR(6),
+ "PAYMENT" CHAR(2),
+ "YEAR" CHAR(4) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
